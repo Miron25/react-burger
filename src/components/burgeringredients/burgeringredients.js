e08b058ffa75d67feefcdbff37c8c216b747fb0c
Мирон Шихов <shikhov.m@yandex.ru>
2023-03-10 07:28:34 +0300

    Modals for ingredients

--- src/components/burgeringredients/burgeringredients.js
+++ src/components/burgeringredients/burgeringredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useMemo } from 'react'
 import BurgerIngStyles from './burgering.module.css'
 import PropTypes from 'prop-types'
 import Modal from './../modal/modal'
@@ -11,22 +11,27 @@ import {
 
 function BurgerIngredients(props) {
   const [current, setCurrent] = useState('one')
-  // const [count, setCount] = useState(0)
   const initial_array = props.mainArray
   const oneRef = useRef(null) //represents tab "one"
   const twoRef = useRef(null) //represents tab "two"
   const threeRef = useRef(null) //represents tab "three"
   const [ind, setInd] = useState(0)
   const [show1, setShow1] = useState(false)
-  //const [bunArray, setBunArray] = useState([])
-
-  /*const handleAddIngredient = useCallback(
-    (obj) => () => {
-      setCount(count + 1)
-      return count
-    },
+  const [show2, setShow2] = useState(false)
+  const [show3, setShow3] = useState(false)
+  const bunArray = useMemo(
+    () => initial_array.filter((ingr) => ingr.type === 'bun'),
     []
-  )*/
+  )
+  const sauceArray = useMemo(
+    () => initial_array.filter((ingr) => ingr.type === 'sauce'),
+    []
+  )
+  const mainArray = useMemo(
+    () => initial_array.filter((ingr) => ingr.type === 'main'),
+    []
+  )
+
   const handleClick = (value) => {
     setInd(value)
     return ind
@@ -38,42 +43,63 @@ function BurgerIngredients(props) {
     ref.current?.scrollIntoView({ behavior: 'smooth' })
   }
 
-  const FilterItems = ({ type }) => {
-    const filtered_array = initial_array
-      .filter((ingr) => ingr.type === type)
-      .map((filteredIngr, index) => (
-        <React.Fragment key={filteredIngr._id}>
-          <div
-            className={BurgerIngStyles.column1}
-            //onClick={handleAddIngredient(filteredIngr)}
-            onClick={() => {
-              setShow1(true)
-              handleClick(index)
-            }}
-            role="button"
-            tabIndex={0}
-            onKeyDown={handleKeyDown}
+  const ModalContent = ({ filtered_array, onClose }) => {
+    return (
+      <>
+        <div className={BurgerIngStyles.popup_title}>
+          <h1
+            className={
+              BurgerIngStyles.popup_header + 'text text_type_main-large'
+            }
           >
-            <Counter count={1} size="default" extraClass="m-1" />
-
-            <img src={filteredIngr.image} alt=""></img>
-            <div className={BurgerIngStyles.pricebox}>
-              <span className="text text_type_digits-default">
-                {filteredIngr.price}
-              </span>
-              <CurrencyIcon />
-            </div>
-            <p
-              className="text text_type_main-default"
-              style={{ textAlign: 'center' }}
-            >
-              {filteredIngr.name}
-            </p>
+            Детали игредиента
+          </h1>
+          <CloseIcon onClick={onClose} />
+        </div>
+        <img
+          src={filtered_array[ind].image}
+          alt=""
+          className={BurgerIngStyles.popup_img}
+        ></img>
+        <h2 className={BurgerIngStyles.popup_name}>
+          {filtered_array[ind].name}
+        </h2>
+        <ul className={BurgerIngStyles.popup_nutrition}>
+          <div className={BurgerIngStyles.popup_nutrition_value}>
+            <span className="text text_type_main-default text_color_inactive">
+              Калории,ккал
+            </span>
+            <span className="text text_type_digits-default">
+              {filtered_array[ind].calories}
+            </span>
           </div>
-        </React.Fragment>
-      ))
-
-    return filtered_array
+          <div className={BurgerIngStyles.popup_nutrition_value}>
+            <span className="text text_type_main-default text_color_inactive">
+              Белки, г
+            </span>
+            <span className="text text_type_digits-default">
+              {filtered_array[ind].proteins}
+            </span>
+          </div>
+          <div className={BurgerIngStyles.popup_nutrition_value}>
+            <span className="text text_type_main-default text_color_inactive">
+              Жиры, г
+            </span>
+            <span className="text text_type_digits-default">
+              {filtered_array[ind].fat}
+            </span>
+          </div>
+          <div className={BurgerIngStyles.popup_nutrition_value}>
+            <span className="text text_type_main-default text_color_inactive">
+              Углеводы, г
+            </span>
+            <span className="text text_type_digits-default">
+              {filtered_array[ind].carbohydrates}
+            </span>
+          </div>
+        </ul>
+      </>
+    )
   }
 
   return (
@@ -126,7 +152,43 @@ function BurgerIngredients(props) {
             Булки
           </p>
           <div className={BurgerIngStyles.grid_block}>
-            <FilterItems type="bun" />
+            {bunArray.map((filteredIngr, index) => (
+              <React.Fragment key={filteredIngr._id}>
+                <div
+                  className={BurgerIngStyles.column1}
+                  //onClick={handleAddIngredient(filteredIngr)}
+                  onClick={() => {
+                    setShow1(true)
+                    handleClick(index)
+                  }}
+                  role="button"
+                  tabIndex={0}
+                  onKeyDown={handleKeyDown}
+                >
+                  <Counter count={1} size="default" extraClass="m-1" />
+
+                  <img src={filteredIngr.image} alt=""></img>
+                  <div className={BurgerIngStyles.pricebox}>
+                    <span className="text text_type_digits-default">
+                      {filteredIngr.price}
+                    </span>
+                    <CurrencyIcon />
+                  </div>
+                  <p
+                    className="text text_type_main-default"
+                    style={{ textAlign: 'center' }}
+                  >
+                    {filteredIngr.name}
+                  </p>
+                </div>
+              </React.Fragment>
+            ))}
+            <Modal show={show1} onClose={() => setShow1(false)}>
+              <ModalContent
+                filtered_array={bunArray}
+                onClose={() => setShow1(false)}
+              />
+            </Modal>
           </div>
           <p
             className="text text_type_main-medium"
@@ -136,7 +198,43 @@ function BurgerIngredients(props) {
             Соусы
           </p>
           <div className={BurgerIngStyles.grid_block}>
-            <FilterItems type="sauce" />
+            {sauceArray.map((filteredIngr, index) => (
+              <React.Fragment key={filteredIngr._id}>
+                <div
+                  className={BurgerIngStyles.column1}
+                  //onClick={handleAddIngredient(filteredIngr)}
+                  onClick={() => {
+                    setShow2(true)
+                    handleClick(index)
+                  }}
+                  role="button"
+                  tabIndex={0}
+                  onKeyDown={handleKeyDown}
+                >
+                  <Counter count={1} size="default" extraClass="m-1" />
+
+                  <img src={filteredIngr.image} alt=""></img>
+                  <div className={BurgerIngStyles.pricebox}>
+                    <span className="text text_type_digits-default">
+                      {filteredIngr.price}
+                    </span>
+                    <CurrencyIcon />
+                  </div>
+                  <p
+                    className="text text_type_main-default"
+                    style={{ textAlign: 'center' }}
+                  >
+                    {filteredIngr.name}
+                  </p>
+                </div>
+              </React.Fragment>
+            ))}
+            <Modal show={show2} onClose={() => setShow2(false)}>
+              <ModalContent
+                filtered_array={sauceArray}
+                onClose={() => setShow2(false)}
+              />
+            </Modal>
           </div>
           <p
             className="text text_type_main-medium"
@@ -146,71 +244,84 @@ function BurgerIngredients(props) {
             Начинки
           </p>
           <div className={BurgerIngStyles.grid_block}>
-            <FilterItems type="main" />
+            {mainArray.map((filteredIngr, index) => (
+              <React.Fragment key={filteredIngr._id}>
+                <div
+                  className={BurgerIngStyles.column1}
+                  //onClick={handleAddIngredient(filteredIngr)}
+                  onClick={() => {
+                    setShow3(true)
+                    handleClick(index)
+                  }}
+                  role="button"
+                  tabIndex={0}
+                  onKeyDown={handleKeyDown}
+                >
+                  <Counter count={1} size="default" extraClass="m-1" />
+
+                  <img src={filteredIngr.image} alt=""></img>
+                  <div className={BurgerIngStyles.pricebox}>
+                    <span className="text text_type_digits-default">
+                      {filteredIngr.price}
+                    </span>
+                    <CurrencyIcon />
+                  </div>
+                  <p
+                    className="text text_type_main-default"
+                    style={{ textAlign: 'center' }}
+                  >
+                    {filteredIngr.name}
+                  </p>
+                </div>
+              </React.Fragment>
+            ))}
+            <Modal show={show3} onClose={() => setShow3(false)}>
+              <ModalContent
+                filtered_array={mainArray}
+                onClose={() => setShow3(false)}
+              />
+            </Modal>
           </div>
         </div>
       </div>
-      <Modal show={show1} onClose={() => setShow1(false)}>
-        <div className={BurgerIngStyles.popup_title}>
-          <h1
-            className={
-              BurgerIngStyles.popup_header + 'text text_type_main-large'
-            }
-          >
-            Детали игредиента
-          </h1>
-          <CloseIcon onClick={() => setShow1(false)} />
-        </div>
-        <img
-          src={initial_array[ind].image}
-          alt=""
-          className={BurgerIngStyles.popup_img}
-        ></img>
-        <h2 className={BurgerIngStyles.popup_name}>
-          {initial_array[ind].name}
-        </h2>
-        <ul className={BurgerIngStyles.popup_nutrition}>
-          <div className={BurgerIngStyles.popup_nutrition_value}>
-            <span className="text text_type_main-default text_color_inactive">
-              Калории,ккал
-            </span>
-            <span className="text text_type_digits-default">
-              {initial_array[ind].calories}
-            </span>
-          </div>
-          <div className={BurgerIngStyles.popup_nutrition_value}>
-            <span className="text text_type_main-default text_color_inactive">
-              Белки, г
-            </span>
-            <span className="text text_type_digits-default">
-              {initial_array[ind].proteins}
-            </span>
-          </div>
-          <div className={BurgerIngStyles.popup_nutrition_value}>
-            <span className="text text_type_main-default text_color_inactive">
-              Жиры, г
-            </span>
-            <span className="text text_type_digits-default">
-              {initial_array[ind].fat}
-            </span>
-          </div>
-          <div className={BurgerIngStyles.popup_nutrition_value}>
-            <span className="text text_type_main-default text_color_inactive">
-              Углеводы, г
-            </span>
-            <span className="text text_type_digits-default">
-              {initial_array[ind].carbohydrates}
-            </span>
-          </div>
-        </ul>
-      </Modal>
     </>
   )
 }
 
 BurgerIngredients.propTypes = {
-  mainArray: PropTypes.array,
-  type: PropTypes.string,
+  mainArray: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      proteins: PropTypes.number.isRequired,
+      fat: PropTypes.number.isRequired,
+      carbohydrates: PropTypes.number.isRequired,
+      calories: PropTypes.number.isRequired,
+      price: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+      image_mobile: PropTypes.string.isRequired,
+      image_large: PropTypes.string.isRequired,
+      __v: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+  filtered_array: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      proteins: PropTypes.number.isRequired,
+      fat: PropTypes.number.isRequired,
+      carbohydrates: PropTypes.number.isRequired,
+      calories: PropTypes.number.isRequired,
+      price: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+      image_mobile: PropTypes.string.isRequired,
+      image_large: PropTypes.string.isRequired,
+      __v: PropTypes.number.isRequired,
+    })
+  ),
+  onClose: PropTypes.func,
 }
 
 export default BurgerIngredients
